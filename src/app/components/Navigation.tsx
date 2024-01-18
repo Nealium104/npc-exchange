7b0d44f201547696b3c9677b3ba89b60d28ab6d0
Neal Grindstaff <neal.grindstaff@gmail.com>
2024-01-18 14:56:39 -0500

    sketched out a navigation bar

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,17 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/characters">Characters</a>
+        </li>
+        <li>
+          <a href="/contact"></a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
